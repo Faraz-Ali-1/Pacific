d19c18554af28330f34f0fb6109ba1ff18583031
Faraz Ali <143830755+Faraz-Ali-1@users.noreply.github.com>
2025-12-12 21:16:51 +0300

    Refactor showCartProducts function for clarity

--- showAddToCart.js
+++ showAddToCart.js
@@ -1,68 +1,99 @@
-import products from "./API/products.JSON";
+import products from "./API/products.json";
+
 import { fetchfromCartLS } from "./fetchfromCartLS.js";
+
 import { getCartProductFromLS } from "./getCartProduct.js";
+
 import { removeProduct } from "./removeProduct.js";
+
 import { updateCartProductTotal } from "./updateCartProductTotal.js";
+
+
 
 let cartProducts = getCartProductFromLS();
 
+
+
 let filterProducts = products.filter((curProd) => {
+
     return cartProducts.some((curElem) => curElem.cardValue === curProd.cardValue);
+
 });
+
+
 
 // console.log(filterProducts);
 
 
+
+
+
 const atcContainer = document.getElementById("atcContainer");
+
 const atcTemplate = document.getElementById("templateAtc");
+
+
+
 
 
 const showCartProducts = () => {
 
-    const response = await fetch("./API/products.json");
-    const products = await response.json();
-
-    let cartProducts = getCartProductFromLS();
-
-    // 2. Filter the products
-    let filterProducts = products.filter((curProd) => {
-        return cartProducts.some((curElem) => curElem.cardValue === curProd.cardValue);
-    });
-
-    // 3. Render
-
     filterProducts.forEach((curProd) => {
+
         const {cardValue, category, name, image, discountedPrice, stockNumber} = curProd;
+
         let productClone = document.importNode(atcTemplate.content, true);
+
+
 
         const LSactualData = fetchfromCartLS(cardValue, discountedPrice);
 
+
+
         productClone.getElementById("cardValue").setAttribute("id", `card${cardValue}`);
+
         productClone.getElementById("category").textContent = category;
+
         productClone.getElementById("image").src = image;
+
         productClone.getElementById("image").alt = name;
+
         productClone.getElementById("name").textContent = name;
 
+
+
         productClone.getElementById("quantityNumber").textContent = LSactualData.quantity;
+
         productClone.getElementById("discountedPrice").textContent = LSactualData.price;
+
+
 
         // increment and decrement button
 
+
+
         productClone.getElementById("RemoveBtn")
+
         .addEventListener("click",() => removeProduct(cardValue));
 
+
+
         atcContainer.append(productClone);
+
     });
+
 };
+
 showCartProducts();
 
+
+
 // -------------------------
+
 // show cart product total
+
 // -------------------------
+
+
 
 updateCartProductTotal();
-
-
-
-
-
